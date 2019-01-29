211f2afff2055c821adc72cb92522b2ee5cb482b
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-01-29 21:58:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -119,7 +119,17 @@ class TicTacToe
     end
   end
   
-  
+  def play
+   until over?
+    turn(board)
+   end
+   if won? && winner == "X"
+    puts "Congratulations X"
+   elsif draw?
+     over?
+     puts "Cat's Game!"
+   end
+  end
   
   
 end
