c17fbf24594696dd1c84bf9c04126eff63c480fa
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-01-29 21:29:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,5 +31,11 @@ class TicTacToe
   @board[index] = player
   end
   
-  
+  def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true 
+  end
+end
 end
